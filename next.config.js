88b7bcabe8b07297e14c796f6e1f28e1bfde142e
subbutgs <93034178+subbutgs@users.noreply.github.com>
2024-06-14 10:11:19 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -2,13 +2,8 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  webpack: (config, { isServer }) => {
-    // Important: return the modified config
-    if (isServer) {
-      config.externals = ["@sparticuz/chromium", ...config.externals];
-    }
-    return config;
+  experimental: {
+    serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
   },
 };
 
